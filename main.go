e04afc9ecc4884ce6c9acb0e44c6925c6e2b1047
server <server>
2020-02-21 14:34:38 +0700

    update tls config

--- main.go
+++ main.go
@@ -74,21 +74,6 @@ func handleServerHTTP(model *models.Http, tmodel *models.Tls, mux *http.ServeMux
 	srv := &http.Server{
 		Addr:    model.Ipv4 + model.Port,
 		Handler: mux,
-		TLSConfig: &tls.Config{
-			MinVersion: tls.VersionTLS12,
-			CurvePreferences: []tls.CurveID{
-				tls.CurveP521,
-				tls.CurveP384,
-				tls.CurveP256,
-			},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
-		},
 		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
 	}
 	srv.ListenAndServeTLS(tmodel.Crt, tmodel.Key)
