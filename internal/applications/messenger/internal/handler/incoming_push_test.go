11a54b9a25730badad75ee8911f463901926f50b
number571 <number571gg@mail.ru>
2024-11-04 20:13:28 +0700

    update

---
+++ internal/applications/messenger/internal/handler/incoming_push_test.go
@@ -0,0 +1,228 @@
+package handler
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/number571/go-peer/pkg/crypto/asymmetric"
+	"github.com/number571/go-peer/pkg/logger"
+	"github.com/number571/hidden-lake/internal/applications/messenger/internal/database"
+	"github.com/number571/hidden-lake/internal/applications/messenger/internal/msgbroker"
+	hls_client "github.com/number571/hidden-lake/internal/service/pkg/client"
+	hls_config "github.com/number571/hidden-lake/internal/service/pkg/config"
+	"github.com/number571/hidden-lake/internal/service/pkg/request"
+	"github.com/number571/hidden-lake/internal/service/pkg/response"
+	hls_settings "github.com/number571/hidden-lake/internal/service/pkg/settings"
+	std_logger "github.com/number571/hidden-lake/internal/utils/logger/std"
+)
+
+func TestHandleIncomingPushHTTP(t *testing.T) {
+	t.Parallel()
+
+	logging, err := std_logger.LoadLogging([]string{})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	httpLogger := std_logger.NewStdLogger(
+		logging,
+		func(_ logger.ILogArg) string {
+			return ""
+		},
+	)
+
+	ctx := context.Background()
+	msgBroker := msgbroker.NewMessageBroker()
+	handler := HandleIncomingPushHTTP(ctx, httpLogger, newTsDatabase(true), msgBroker, newTsHLSClient(true))
+
+	if err := incomingPushRequestOK(handler); err != nil {
+		t.Error(err)
+		return
+	}
+
+	if err := incomingPushRequestMethod(handler); err == nil {
+		t.Error("request success with invalid method")
+		return
+	}
+	if err := incomingPushRequestPubKey(handler); err == nil {
+		t.Error("request success with invalid pubkey")
+		return
+	}
+	if err := incomingPushRequestMessage(handler); err == nil {
+		t.Error("request success with invalid message")
+		return
+	}
+
+	handlerx := HandleIncomingPushHTTP(ctx, httpLogger, newTsDatabase(true), msgBroker, newTsHLSClient(false))
+	if err := incomingPushRequestOK(handlerx); err == nil {
+		t.Error("request success with invalid my pubkey")
+		return
+	}
+	handlery := HandleIncomingPushHTTP(ctx, httpLogger, newTsDatabase(false), msgBroker, newTsHLSClient(true))
+	if err := incomingPushRequestOK(handlery); err == nil {
+		t.Error("request success with invalid push message")
+		return
+	}
+}
+
+func incomingPushRequestOK(handler http.HandlerFunc) error {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/push", bytes.NewBuffer(wrapText("hello, world!")))
+	req.Header.Set(hls_settings.CHeaderPublicKey, asymmetric.NewPrivKey().GetPubKey().ToString())
+
+	handler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("bad status code") // nolint: err113
+	}
+
+	if _, err := io.ReadAll(res.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func incomingPushRequestMessage(handler http.HandlerFunc) error {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/push", nil)
+	req.Header.Set(hls_settings.CHeaderPublicKey, asymmetric.NewPrivKey().GetPubKey().ToString())
+
+	handler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("bad status code") // nolint: err113
+	}
+
+	if _, err := io.ReadAll(res.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func incomingPushRequestPubKey(handler http.HandlerFunc) error {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/push", bytes.NewBuffer(wrapText("hello, world!")))
+
+	handler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("bad status code") // nolint: err113
+	}
+
+	if _, err := io.ReadAll(res.Body); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func incomingPushRequestMethod(handler http.HandlerFunc) error {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/push", nil)
+
+	handler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("bad status code") // nolint: err113
+	}
+
+	return nil
+}
+
+var (
+	_ hls_client.IClient = &tsHLSClient{}
+)
+
+type tsHLSClient struct {
+	fGetPubKey bool
+	fPrivKey   asymmetric.IPrivKey
+}
+
+func newTsHLSClient(pGetPubKey bool) *tsHLSClient {
+	return &tsHLSClient{
+		fGetPubKey: pGetPubKey,
+		fPrivKey:   asymmetric.NewPrivKey(),
+	}
+}
+
+func (p *tsHLSClient) GetIndex(context.Context) (string, error) { return "", nil }
+func (p *tsHLSClient) GetSettings(context.Context) (hls_config.IConfigSettings, error) {
+	return nil, nil
+}
+
+func (p *tsHLSClient) GetPubKey(context.Context) (asymmetric.IPubKey, error) {
+	if !p.fGetPubKey {
+		return nil, errors.New("some error") // nolint: err113
+	}
+	return p.fPrivKey.GetPubKey(), nil
+}
+
+func (p *tsHLSClient) GetOnlines(context.Context) ([]string, error) { return nil, nil }
+func (p *tsHLSClient) DelOnline(context.Context, string) error      { return nil }
+
+func (p *tsHLSClient) GetFriends(context.Context) (map[string]asymmetric.IPubKey, error) {
+	return nil, nil
+}
+
+func (p *tsHLSClient) AddFriend(context.Context, string, asymmetric.IPubKey) error { return nil }
+func (p *tsHLSClient) DelFriend(context.Context, string) error                     { return nil }
+
+func (p *tsHLSClient) GetConnections(context.Context) ([]string, error) { return nil, nil }
+func (p *tsHLSClient) AddConnection(context.Context, string) error      { return nil }
+func (p *tsHLSClient) DelConnection(context.Context, string) error      { return nil }
+
+func (p *tsHLSClient) BroadcastRequest(context.Context, string, request.IRequest) error {
+	return nil
+}
+
+func (p *tsHLSClient) FetchRequest(context.Context, string, request.IRequest) (response.IResponse, error) {
+	return nil, nil
+}
+
+type tsDatabase struct {
+	fPushOK bool
+	fMsg    database.IMessage
+}
+
+func newTsDatabase(pPushOK bool) *tsDatabase {
+	return &tsDatabase{
+		fPushOK: pPushOK,
+	}
+}
+
+func (p *tsDatabase) Close() error { return nil }
+
+func (p *tsDatabase) Size(database.IRelation) uint64 {
+	if p.fMsg == nil {
+		return 0
+	}
+	return 1
+}
+
+func (p *tsDatabase) Push(_ database.IRelation, pM database.IMessage) error {
+	if !p.fPushOK {
+		return errors.New("some error") // nolint: err113
+	}
+	p.fMsg = pM
+	return nil
+}
+
+func (p *tsDatabase) Load(database.IRelation, uint64, uint64) ([]database.IMessage, error) {
+	return []database.IMessage{p.fMsg}, nil
+}
