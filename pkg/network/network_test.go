6da38aa8ad214141bceacd7e2da372c6a4c353a8
number571 <number571gg@mail.ru>
2024-11-12 05:11:00 +0700

    update

--- pkg/network/network_test.go
+++ pkg/network/network_test.go
@@ -2,6 +2,79 @@ package network
 
 import (
 	"testing"
+	"time"
+
+	"github.com/number571/go-peer/pkg/crypto/asymmetric"
+	hiddenlake "github.com/number571/hidden-lake"
 )
 
-func TestNoting(_ *testing.T) {}
+func TestPanicNode(t *testing.T) {
+	t.Parallel()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("nothing panics")
+			return
+		}
+	}()
+
+	_ = NewHiddenLakeNode(
+		"name",
+		NewSettings(&SSettings{
+			FMessageSizeBytes: 4570,
+			FQueuePeriod:      time.Second,
+			FFetchTimeout:     time.Second,
+		}),
+		asymmetric.NewPrivKey(),
+		&tsDatabase{},
+	)
+}
+
+func TestPanicSettings(t *testing.T) {
+	t.Parallel()
+
+	for i := 0; i < 4; i++ {
+		testPanicSettings(t, i)
+	}
+}
+
+func testPanicSettings(t *testing.T, n int) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("nothing panics")
+			return
+		}
+	}()
+	switch n {
+	case 0:
+		_ = NewSettingsByNetworkKey("__test_unknown__", nil)
+	case 1:
+		_ = NewSettings(&SSettings{
+			FMessageSizeBytes: 8192,
+			FQueuePeriod:      time.Second,
+		})
+	case 2:
+		_ = NewSettings(&SSettings{
+			FMessageSizeBytes: 8192,
+			FFetchTimeout:     time.Second,
+		})
+	case 3:
+		_ = NewSettings(&SSettings{
+			FQueuePeriod:  time.Second,
+			FFetchTimeout: time.Second,
+		})
+	}
+}
+
+func TestSettings(t *testing.T) {
+	t.Parallel()
+
+	_ = NewSettingsByNetworkKey(hiddenlake.CDefaultNetwork, nil)
+}
+
+type tsDatabase struct{}
+
+func (p *tsDatabase) Close() error               { return nil }
+func (p *tsDatabase) Set([]byte, []byte) error   { return nil }
+func (p *tsDatabase) Get([]byte) ([]byte, error) { return nil, nil }
+func (p *tsDatabase) Del([]byte) error           { return nil }
