759b173901d2d801a40b74d6b50caef6fd8ab177
number571 <number571gg@mail.ru>
2024-11-23 08:39:49 +0700

    update

--- build/build_test.go
+++ build/build_test.go
@@ -34,10 +34,47 @@ func TestHiddenLakeNetworks(t *testing.T) {
 		t.Error("network.FQueuePeriodMS != 5_000")
 		return
 	}
+
+	switch {
+	case network.GetFetchTimeout() != time.Duration(60_000)*time.Millisecond:
+		fallthrough
+	case network.GetQueuePeriod() != time.Duration(5_000)*time.Millisecond:
+		t.Error("Get methods (networks) is not valid")
+	}
 }
 
 func TestHiddenLakeSettings(t *testing.T) {
 	t.Parallel()
+
+	settings := SSettings{}
+	if err := settings.validate(); err == nil {
+		t.Error("success validate with invalid queue capacity")
+		return
+	}
+
+	settings.FQueueCapacity.FMain = 64
+	settings.FQueueCapacity.FRand = 64
+	if err := settings.validate(); err == nil {
+		t.Error("success validate with invalid network manager")
+		return
+	}
+
+	settings.FNetworkManager.FCacheHashesCap = 2048
+	settings.FNetworkManager.FConnectsLimiter = 128
+	settings.FNetworkManager.FKeeperPeriodMS = 5_000
+	if err := settings.validate(); err == nil {
+		t.Error("success validate with invalid network connection")
+		return
+	}
+
+	settings.FNetworkConnection.FDialTimeoutMS = 5_000
+	settings.FNetworkConnection.FReadTimeoutMS = 5_000
+	settings.FNetworkConnection.FWriteTimeoutMS = 5_000
+	settings.FNetworkConnection.FWaitTimeoutMS = 5_000_000
+	if err := settings.validate(); err != nil {
+		t.Error(err)
+		return
+	}
 
 	if GSettings.FProtoMask.FNetwork != 0x5f67705f {
 		t.Error(`GSettings.ProtoMask.Network != 0x5f67705f`)
@@ -93,6 +130,6 @@ func TestHiddenLakeSettings(t *testing.T) {
 	case GSettings.GetWriteTimeout() != time.Duration(GSettings.FNetworkConnection.FWriteTimeoutMS)*time.Millisecond:
 		fallthrough
 	case GSettings.GetKeeperPeriod() != time.Duration(GSettings.FNetworkManager.FKeeperPeriodMS)*time.Millisecond:
-		t.Error("Get methods is not valid")
+		t.Error("Get methods (settings) is not valid")
 	}
 }
