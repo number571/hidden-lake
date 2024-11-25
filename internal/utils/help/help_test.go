9efd4371241344c740882a1113da6cb700659644
number571 <number571gg@mail.ru>
2024-11-25 18:26:08 +0700

    update

--- internal/utils/help/help_test.go
+++ internal/utils/help/help_test.go
@@ -1,5 +1,24 @@
 package help
 
-import "testing"
+const (
+	yamlString = `name: Hidden Lake Service (HLS)
+desc: anonymizes traffic using the QB-problem
+args: |
+  [ -h, --help    ] - print information about service
+  [ -v, --version ] - print version of service
+  [ -p, --path    ] - set path to config, database files
+  [ -n, --network ] - set network key for connections
+  [ -t, --threads ] - set num of parallel functions to calculate PoW`
+)
 
-func TestNothing(_ *testing.T) {}
+func ExamplePrintln() {
+	Println([]byte(yamlString))
+	// Output:
+	// Hidden Lake Service (HLS)
+	// anonymizes traffic using the QB-problem
+	// [ -h, --help    ] - print information about service
+	// [ -v, --version ] - print version of service
+	// [ -p, --path    ] - set path to config, database files
+	// [ -n, --network ] - set network key for connections
+	// [ -t, --threads ] - set num of parallel functions to calculate PoW
+}
