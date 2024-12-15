7a04bbb7a8de37b5eaf1ed1164ca39d1394cf2e7
number571 <number571gg@mail.ru>
2024-12-15 07:13:12 +0700

    update

--- internal/adapters/tcp/pkg/app/app_test.go
+++ internal/adapters/tcp/pkg/app/app_test.go
@@ -60,6 +60,36 @@ func TestInitApp(t *testing.T) {
 
 func testDeleteFiles(path string) {
 	os.RemoveAll(path + settings.CPathYML)
+	os.RemoveAll(path + settings.CPathDB)
+}
+
+func TestAppDatabase(t *testing.T) {
+	t.Parallel()
+
+	// Run application
+	cfg, err := config.BuildConfig(settings.CPathYML, &config.SConfig{
+		FSettings: &config.SConfigSettings{
+			FMessageSizeBytes: 8192,
+			FWorkSizeBits:     10,
+			FNetworkKey:       "_",
+			FDatabaseEnabled:  true,
+		},
+		FAddress: &config.SAddress{
+			FInternal: testutils.TgAddrs[17],
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	app := NewApp(cfg, ".")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() { _ = app.Run(ctx) }()
+	time.Sleep(100 * time.Millisecond)
 }
 
 func TestApp(t *testing.T) {
