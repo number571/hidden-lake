68b1062795b44828e03a539f6eb827f743d50776
number571 <number571gg@mail.ru>
2024-12-15 14:54:46 +0700

    update

--- cmd/hla/hla_tcp/daemon/install_hla_tcp.sh
+++ cmd/hla/hla_tcp/daemon/install_hla_tcp.sh
@@ -6,7 +6,7 @@ echo "
 Description=HiddenLakeAdapterTCP
 
 [Service]
-ExecStart=/root/hla_tcp_amd64_linux -path=/root -threads=1
+ExecStart=/root/hla_tcp_amd64_linux -path=/root
 Restart=always
 RestartSec=10
 
