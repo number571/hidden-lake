aefab2fcb35efac5c3f157f9d5df29208ce22689
number571 <number571gg@mail.ru>
2024-12-15 07:07:24 +0700

    update

--- pkg/adapters/http/http_test.go
+++ pkg/adapters/http/http_test.go
@@ -1,9 +1,12 @@
 package http
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 	"time"
 
@@ -20,18 +23,43 @@ import (
 func TestHTTPAdapter(t *testing.T) {
 	t.Parallel()
 
-	adapter := NewHTTPAdapter(
+	adapterSettings := adapters.NewSettings(&adapters.SSettings{
+		FMessageSizeBytes: 8192,
+	})
+
+	adapter3 := NewHTTPAdapter(
 		NewSettings(&SSettings{
-			FAdapterSettings: adapters.NewSettings(&adapters.SSettings{
-				FMessageSizeBytes: 8192,
-			}),
-			FAddress: testutils.TgAddrs[18],
+			FAdapterSettings: adapterSettings,
+		}),
+		cache.NewLRUCache(1),
+		func() []string { return nil },
+	)
+
+	adapter2 := NewHTTPAdapter(
+		NewSettings(&SSettings{
+			FAdapterSettings: adapterSettings,
+			FAddress:         testutils.TgAddrs[19],
 		}),
 		cache.NewLRUCache(1024),
 		func() []string { return nil },
 	)
 
-	adapter.WithHandlers(
+	adapter1 := NewHTTPAdapter(
+		NewSettings(&SSettings{
+			FAdapterSettings: adapterSettings,
+			FAddress:         testutils.TgAddrs[18],
+		}),
+		cache.NewLRUCache(1024),
+		func() []string { return []string{testutils.TgAddrs[19]} },
+	)
+
+	onlines := adapter1.GetOnlines()
+	if len(onlines) != 1 || onlines[0] != testutils.TgAddrs[19] {
+		t.Error("adapter: get onlines")
+		return
+	}
+
+	adapter1.WithHandlers(
 		NewHandler(
 			settings.CHandleIndexPath,
 			func(w http.ResponseWriter, _ *http.Request) { fmt.Fprint(w, "http-adapter") },
@@ -53,7 +81,9 @@ func TestHTTPAdapter(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	go func() { _ = adapter.Run(ctx) }()
+	go func() { _ = adapter3.Run(ctx) }()
+	go func() { _ = adapter2.Run(ctx) }()
+	go func() { _ = adapter1.Run(ctx) }()
 
 	client := client.NewClient(
 		client.NewRequester(testutils.TgAddrs[18], &http.Client{Timeout: 5 * time.Second}),
@@ -89,7 +119,7 @@ func TestHTTPAdapter(t *testing.T) {
 		return
 	}
 
-	onlines, err := client.GetOnlines(ctx)
+	onlines, err = client.GetOnlines(ctx)
 	if err != nil {
 		t.Error(err)
 		return
@@ -112,17 +142,76 @@ func TestHTTPAdapter(t *testing.T) {
 		return
 	}
 
+	msgBytes := []byte("hello, world!")
+	msgBytes = append(msgBytes, random.NewRandom().GetBytes(uint64(8192-len(msgBytes)))...)
 	netMsg := message.NewMessage(
 		message.NewConstructSettings(&message.SConstructSettings{
 			FSettings: message.NewSettings(&message.SSettings{}),
 		}),
-		payload.NewPayload32(0x01, random.NewRandom().GetBytes(8192)),
+		payload.NewPayload32(0x01, msgBytes),
 	)
 
 	if err := client.ProduceMessage(ctx, netMsg); err != nil {
 		t.Error(err)
 		return
 	}
+	if err := client.ProduceMessage(ctx, netMsg); err == nil {
+		t.Error("success produce message duplicate")
+		return
+	}
+
+	netMsg2 := message.NewMessage(
+		message.NewConstructSettings(&message.SConstructSettings{
+			FSettings: message.NewSettings(&message.SSettings{}),
+		}),
+		payload.NewPayload32(0x01, []byte{1}),
+	)
+	if err := client.ProduceMessage(ctx, netMsg2); err == nil {
+		t.Error("success produce invalid size message")
+		return
+	}
+
+	if err := testCustomProduceMessage(ctx, http.MethodGet, testutils.TgAddrs[18], netMsg.ToString()); err == nil {
+		t.Error("success produce message with invalid method")
+		return
+	}
+	invalidMsg := random.NewRandom().GetString(8192 << 1)
+	if err := testCustomProduceMessage(ctx, http.MethodPost, testutils.TgAddrs[18], invalidMsg); err == nil {
+		t.Error("success produce message with invalid message")
+		return
+	}
+
+	msg, err := adapter1.Consume(ctx)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !bytes.HasPrefix(msg.GetPayload().GetBody(), msgBytes) {
+		t.Error("get invalid message bytes")
+		return
+	}
+
+	if err := adapter1.Produce(ctx, msg); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func testCustomProduceMessage(ctx context.Context, method, host, msg string) error {
+	req, err := http.NewRequestWithContext(ctx, method, "http://"+host, strings.NewReader(msg))
+	if err != nil {
+		return err
+	}
+	client := http.Client{Timeout: 5 * time.Second}
+	rsp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer rsp.Body.Close()
+	if rsp.StatusCode != http.StatusOK {
+		return errors.New("bad status code") // nolint: err113
+	}
+	return nil
 }
 
 func TestHandler(t *testing.T) {
