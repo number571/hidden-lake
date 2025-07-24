3489a026dfd8ad7b2a1511a7cd40129d9200c425
number571 <number571@example.com>
2025-07-24 12:54:40 +0700

    update

--- cmd/hlk/main.go
+++ cmd/hlk/main.go
@@ -12,8 +12,8 @@ import (
 	"github.com/number571/hidden-lake/build"
 	"github.com/number571/hidden-lake/internal/kernel/pkg/app"
 	"github.com/number571/hidden-lake/internal/kernel/pkg/settings"
-	"github.com/number571/hidden-lake/internal/utils/help"
 	"github.com/number571/hidden-lake/pkg/utils/flag"
+	"github.com/number571/hidden-lake/pkg/utils/help"
 )
 
 var (
